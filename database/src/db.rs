63cdd6f5cc3b96112cd9a1558f97e1b9255e63ea
max143672 <biryukov.maxim.vladimirovich@yandex.ru>
2023-07-20 18:08:52 +0400

    "Enhance database options for performance tuning"
    This commit modifies the database options in the 'db.rs' file for performance improvements. It includes additional options like 'BlockBasedTableFactory' using 'BlockBasedOptions' and the 'bloom_filter' to reduce disk I/O. Moreover, a set of write buffer configurations have been added to better control write operations, and 'max_bytes_for_level_base' has been set for controlling the size of level-1. It also enables database statistics and sets a period for their reporting, aiding in performance monitoring. Finally, it keeps the maximum open files to 500 to maintain a balance with most Linux environments.

--- database/src/db.rs
+++ database/src/db.rs
@@ -1,4 +1,4 @@
-use rocksdb::{DBWithThreadMode, MultiThreaded};
+use rocksdb::{BlockBasedOptions, DBWithThreadMode, MultiThreaded};
 use std::{path::PathBuf, sync::Arc};
 
 /// The DB type used for Kaspad stores
@@ -10,9 +10,26 @@ pub fn open_db(db_path: PathBuf, create_if_missing: bool, parallelism: usize) ->
     if parallelism > 1 {
         opts.increase_parallelism(parallelism as i32);
     }
+
+    let mut b_opts = BlockBasedOptions::default();
+    b_opts.set_bloom_filter(2.0, true);
+    opts.set_block_based_table_factory(&b_opts);
+
+    opts.set_write_buffer_size(256 * 1024 * 1024);
+    opts.set_max_write_buffer_number(3);
+    opts.set_min_write_buffer_number(2);
+    opts.set_min_write_buffer_number_to_merge(2);
+    opts.set_max_bytes_for_level_base(1024 * 1024 * 1024);
+
     // In most linux environments the limit is set to 1024, so we use 500 to give sufficient slack.
     // TODO: fine-tune this parameter and additional parameters related to max file size
     opts.set_max_open_files(500);
+
+    // metrics
+    opts.enable_statistics();
+    opts.set_report_bg_io_stats(true);
+    opts.set_stats_dump_period_sec(dbg!(std::env::var("SDPS").unwrap_or("600".to_string()).parse().unwrap_or(600)));
+
     opts.create_if_missing(create_if_missing);
     let db = Arc::new(DB::open(&opts, db_path.to_str().unwrap()).unwrap());
     db
